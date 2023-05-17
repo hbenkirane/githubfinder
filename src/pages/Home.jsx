7ebc65d7adaa702c0b4023f9dc78d8271af18559
Hicham Benkirane <hi.benkirane@gmail.com>
2023-05-17 18:54:52 +0200

    add the components@

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
+
+function Home(props) {
+  return (
+    <>
+      <UserSearch />
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
